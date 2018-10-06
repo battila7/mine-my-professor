7514a0735686d5f13b45d88988d1c81a65512dba
Attila Bagossy <bagossyattila@outlook.com>
2018-10-06 11:30:05 +0200

    Implemented the pipeline driver

---
+++ cleanser/src/pipeline/index.js
@@ -0,0 +1,21 @@
+const stages = [
+    'mergeTeachers'
+];
+
+function process(options, initialDataset) {
+    const stageFunctions = stages.map(name => executeStage.bind(null, name, options.stages[name]));
+
+    return stageFunctions.reduce(
+        (promise, stageFn) => promise.then(resultDataset => stageFn(resultDataset)),
+        Promise.resolve(initialDataset));
+};
+
+function executeStage(stageName, options, dataset) {
+    const stage = require(`./${stageName}`);
+
+    return stage.process(options, dataset)
+}
+
+module.exports = {
+    process
+};
