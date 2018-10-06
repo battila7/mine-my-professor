f8d573a32ddda17ae553eee61323365f47ffc66c
Attila Bagossy <bagossyattila@outlook.com>
2018-10-06 09:19:54 +0200

    Extracted some common mining logic into miner

---
+++ scraper/src/mine/Miner.js
@@ -0,0 +1,16 @@
+const SequentialDownloader = require('../download/SequentialDownloader');
+
+const Miner = {
+    Miner(baseUri) {
+        this.baseUri = baseUri;
+
+        this.downloader = Object.create(SequentialDownloader);
+        this.downloader.SequentialDownloader(this.baseUri, this.downloaderOnProgress.bind(this));
+    },
+    mine() {
+        return this.downloader.downloadAll()
+            .then(() => this.onCompleted());
+    }
+};
+
+module.exports = Miner;
